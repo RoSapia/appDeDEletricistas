cc6bce902bda390f04bb4577395c407a183f72d9
RoSapia <roberta.sapia@hotmail.com>
2024-11-12 19:25:25 -0300

    ajuste em approutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -7,17 +7,17 @@ import RegistrarAtendimento from '../pages/RegistrarAtendimento';
 import Home from '../pages/Home';
 
 function AppRoutes() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/agendamentos" element={<Agendamentos />} />
-        <Route path="/registrar-atendimento" element={<RegistrarAtendimento />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router basename="/">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/cadastro" element={<Cadastro />} />
+                <Route path="/agendamentos" element={<Agendamentos />} />
+                <Route path="/registrar-atendimento" element={<RegistrarAtendimento />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default AppRoutes;
