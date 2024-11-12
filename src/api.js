ba25cb87a29409ed0ff8fdb057f1b04a2132847e
RoSapia <roberta.sapia@hotmail.com>
2024-11-12 14:27:38 -0300

    Adicionando link backend

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8080/api"
+    //baseURL: "http://localhost:8080/api"
+    baseURL: "https://dedeletricistasbackend-production.up.railway.app/api/"
 })
 
 export default api;
